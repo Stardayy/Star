f675186588c6910be5c6717ea2778aefc4ad5f78
Stardayy <triben21806@gmail.com>
2025-06-19 03:46:52 +0700

    Create main

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f0(i,n) for(int i=0;i<n;++i)
+#define f1(i,n) for(int i=1;i<=n;++i)
+#define fi first
+#define se second
+#define pb push_back
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pair<int,int> > vpi;
+typedef vector<long long> vll;
+typedef vector<bool> vb;
+const ll mod = (ll)1e9 + 7;
+const int MAXN = (int)1e5 + 4;
+
+void Solve()
+{
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+    {
+        Solve();
+    }
+    return 0;
+}
